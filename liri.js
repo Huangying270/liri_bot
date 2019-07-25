fba5cfe00ab445b0b33636b556d48a1a26745080
Ying Huang <huangying270@gmail.com>
2019-07-25 14:50:51 -0400

    added npm files and spotify api

--- liri.js
+++ liri.js
@@ -2,4 +2,12 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 
+var fs = require("fs");
+
+var axios = require("axios");
+
 var spotify = new Spotify(keys.spotify);
+
+// "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+
+// "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy"
